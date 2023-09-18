343aa9689f626e5bfe44eaf62b7caa935dfa5d04
niccov <niccoventuri27@gmail.com>
2023-09-18 12:12:01 +0200

    Milestone 2 completata

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -14,6 +14,41 @@
             Console.Write("Inserisci la capienza massima: ");
             evento.capienzaMassima = int.Parse(Console.ReadLine());
 
+            Console.Write("Quanti posti vuoi prenotare? ");
+            int postiDaPrenotare = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            evento.PrenotaPosti(postiDaPrenotare);
+            Console.WriteLine($"Numero di posti prenotati = {postiDaPrenotare}");
+            Console.WriteLine($"Numero di posti disponibili = {evento.CapienzaMassima - postiDaPrenotare}");
+
+            while (true)
+            {
+                Console.Write("Vuoi disdire dei posti? (Sì/No): ");
+                string risposta = Console.ReadLine().Trim().ToLower();
+
+                if (risposta == "no")
+                {
+                    break;
+                }   
+                else if (risposta == "sì" || risposta == "si")
+                {
+                    Console.Write("Indica il numero di posti da disdire: ");
+                    int postiDaDisdire = int.Parse(Console.ReadLine());
+
+                    evento.DisdiciPosti(postiDaDisdire);
+                    Console.WriteLine($"Disdetti {postiDaDisdire} posti.");
+                }
+                else
+                {
+                    Console.WriteLine("Risposta non valida. Rispondi con 'Sì' o 'No'.");
+                }
+
+                Console.WriteLine($"Posti prenotati dopo le disdette: {evento.postiPrenotati}");
+                Console.WriteLine($"Posti disponibili dopo le disdette: {evento.CapienzaMassima - evento.postiPrenotati}");
+            }
+
         }
     }
 }
